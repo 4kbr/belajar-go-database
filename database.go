1b4e5c81e69ce710accd797d87f64ec0eec37fc4
4kbr <igyutubfesbuk@gmail.com>
2023-08-31 17:09:30 +0700

    test prepare statement

--- database.go
+++ database.go
@@ -6,7 +6,7 @@ import (
 )
 
 func GetConnection() *sql.DB {
-	db, err := sql.Open("mysql", "root:root@tcp(localhost:8889)/belajar_golang_database")
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:8889)/belajar_golang_database?parseTime=true")
 	if err != nil {
 		panic(err)
 	}
